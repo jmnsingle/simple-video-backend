38c4a488eb603bbf7b01fe191ad7efb16ac37f8e
Juliano <jmnogueira2011@gmail.com>
2020-04-07 17:17:42 -0300

    Backend created.

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const users = {};
+
+io.on('connection', socket => {
+
+  if (!users[socket.id]) {
+    users[socket.id] = socket.id;
+  }
+
+  socket.emit("yourID", socket.id);
+  io.sockets.emit("allUsers", users);
+
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
+  });
+
+  socket.on("acceptCall", (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
+
+});
+
+server.listen(3333, () => console.log('Server is runnning on port 3333'));
